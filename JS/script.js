8bd423fcd5dba27225732c5c51a5b061fdb28e78
mesachin <74847785+mepansachin@users.noreply.github.com>
2021-12-03 18:57:53 +0530

    first commit

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "Sachin";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let sachin_subugade = "Sachin Subugade";
+console.log(sachin_subugade);
+
+let $function = 27;
+let person = "Sachin";
+
+let myFirstJob = "Programmer";
+console.log(myFirstJob);
