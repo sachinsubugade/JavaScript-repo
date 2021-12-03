adc2362f335f2c78af39615d7ce1c0471ce2d92f
mesachin <74847785+mepansachin@users.noreply.github.com>
2021-12-03 23:19:16 +0530

    updated files

---
+++ JS/assignment1.js
@@ -0,0 +1,7 @@
+let country = "India";
+let continent = "Asia";
+let population = 1378;
+
+console.log(continent);
+console.log(country);
+console.log(population);
--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+/*
+////////////// VALUES AND VARIABLES ///////////////
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -15,3 +17,71 @@ let person = "Sachin";
 
 let myFirstJob = "Programmer";
 console.log(myFirstJob);
+
+
+////////////// DATA TYPES //////////////
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+console.log(typeof "Sachin");
+console.log(typeof 24);
+console.log(typeof true);
+
+javascriptIsFun = "Yes !";
+console.log(typeof javascriptIsFun)
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1997;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+
+let age = 23;
+age = 24;
+
+const birthYear = 1997;
+// birthYear = 1996;
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+firstName = "Sachin";
+console.log(firstName);
+
+*/
+
+// Mathematical Operators
+const now = 2021;
+const ageSachin = now - 1997;
+
+console.log(ageSachin , ageSachin);
+console.log(ageSachin * 2);
+console.log(ageSachin / 2);
+console.log(2 ** 3);
+
+const firstName = "Sachin";
+const lastName = "Subugade";
+console.log(firstName + ' ' + lastName);
+
+// Assignment Operators
+let x = 5 + 10;
+x += 20;
+x *= 2;
+x /= 2;
+x++;
+x--;
+x--;
+console.log(x);
+
+// Comparision Values
+console.log(ageSachin > 18);
+console.log(ageSachin > 50);
+
+console.log(now - 1997 > now - 2000);
